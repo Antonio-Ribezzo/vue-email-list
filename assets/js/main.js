8ce2df9f4565b66d4730c3e5dbc2ec4d5e55ff1d
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-04-21 16:04:07 +0200

    Array with ten random mail on page start

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,24 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        arrayMail: []
+      }
+    },
+
+    mounted(){
+    for(let i = 0; i < 10; i++){
+      this.generateMailRandom()
+    }
+       
+    },
+
+    methods: {
+      generateMailRandom(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+          this.arrayMail.push(res.data.response);
+        })
       }
     }
+
   }).mount('#app')
